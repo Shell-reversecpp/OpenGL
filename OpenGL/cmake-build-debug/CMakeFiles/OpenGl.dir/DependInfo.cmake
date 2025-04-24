90daf56444f5d2df1095f54b77e1971df5a8dff3
shell_0x8 <sheldoncp73@gmail.com>
2025-04-24 15:19:49 +0100

    Test task

---
+++ OpenGL/cmake-build-debug/CMakeFiles/OpenGl.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/ControlPanel.cpp" "CMakeFiles/OpenGl.dir/ControlPanel.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/ControlPanel.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/GLCanvas.cpp" "CMakeFiles/OpenGl.dir/GLCanvas.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/GLCanvas.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/Game.cpp" "CMakeFiles/OpenGl.dir/Game.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/Game.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/GameWindow.cpp" "CMakeFiles/OpenGl.dir/GameWindow.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/GameWindow.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/IButtonListener.cpp" "CMakeFiles/OpenGl.dir/IButtonListener.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/IButtonListener.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/Shaders.cpp" "CMakeFiles/OpenGl.dir/Shaders.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/Shaders.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/UI.cpp" "CMakeFiles/OpenGl.dir/UI.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/UI.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/UIButton.cpp" "CMakeFiles/OpenGl.dir/UIButton.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/UIButton.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/UIElement.cpp" "CMakeFiles/OpenGl.dir/UIElement.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/UIElement.cpp.o.d"
+  "/home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/WidgetWindow.cpp" "CMakeFiles/OpenGl.dir/WidgetWindow.cpp.o" "gcc" "CMakeFiles/OpenGl.dir/WidgetWindow.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenGL/cmake-build-debug/CMakeFiles/OpenGl.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGl.dir/ControlPanel.cpp.o"
+  "CMakeFiles/OpenGl.dir/ControlPanel.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/GLCanvas.cpp.o"
+  "CMakeFiles/OpenGl.dir/GLCanvas.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/Game.cpp.o"
+  "CMakeFiles/OpenGl.dir/Game.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/GameWindow.cpp.o"
+  "CMakeFiles/OpenGl.dir/GameWindow.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/IButtonListener.cpp.o"
+  "CMakeFiles/OpenGl.dir/IButtonListener.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/Shaders.cpp.o"
+  "CMakeFiles/OpenGl.dir/Shaders.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/UI.cpp.o"
+  "CMakeFiles/OpenGl.dir/UI.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/UIButton.cpp.o"
+  "CMakeFiles/OpenGl.dir/UIButton.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/UIElement.cpp.o"
+  "CMakeFiles/OpenGl.dir/UIElement.cpp.o.d"
+  "CMakeFiles/OpenGl.dir/WidgetWindow.cpp.o"
+  "CMakeFiles/OpenGl.dir/WidgetWindow.cpp.o.d"
+  "OpenGl"
+  "OpenGl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
