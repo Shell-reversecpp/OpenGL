ed958629a8bd39c7291b43942d8479377831bd46
shell_0x8 <54714493+Shell-reversecpp@users.noreply.github.com>
2025-04-24 15:12:02 +0100

    Delete OpenGL/cmake-build-debug directory

--- OpenGL/cmake-build-debug/CMakeFiles/OpenGl.dir/IButtonListener.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/OpenGl.dir/IButtonListener.cpp.o: \
- /home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/IButtonListener.cpp \
- /usr/include/stdc-predef.h \
- /home/snake0x8/CLionProjects/InteractiveGLCanvaswithControls/OpenGL/IButtonListener.h
